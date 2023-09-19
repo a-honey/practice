513c3ebbbb07511dab1d47bea230d6aa6dcde770
a-honey <jungahyeon0512@khu.ac.kr>
2023-09-20 01:52:55 +0900

    :sparkles: Feat: Django 라우팅 설정

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+  return HttpResponse('Hello:D')
+
+def hello(request, id):
+  return HttpResponse('안녕하세요, ' + id + '님!')
